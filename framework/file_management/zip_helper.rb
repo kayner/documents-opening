b13b5f53175233397c6d1bdd049bcd0fb03cd9d4
Egor Rozhentsov <egor.rozhentsov@gmail.com>
2020-01-23 18:37:30 +0300

    Added ZIPHelper

---
+++ framework/file_management/zip_helper.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+# Class for zipping folders
+class ZIPHelper
+  # @param [String] folder_path, Path to folder for zipping
+  # @param [Object] zip_path, Path for saving zip
+  def self.compress(folder_path, zip_path)
+    Zip::File.open(zip_path, Zip::File::CREATE) do |zipfile|
+      Dir[File.join(folder_path, '*')].each do |file|
+        zipfile.add(file.sub(folder_path + '/', ''), file)
+      end
+    end
+  end
+end
