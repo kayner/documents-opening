4ed514929b9f70b6cf6c7f76626f73f459808b3f
Egor Rozhentsov <egor.rozhentsov@gmail.com>
2020-01-28 11:30:39 +0300

    Added file_management

---
+++ framework/file_management.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+require_relative 'file_management/config_helper'
+require_relative 'file_management/dump_creator'
+require_relative 'file_management/file_helper'
+require_relative 'file_management/zip_helper'
