67751ee1eb23a79ceb938f92337ef678475ce86f
Egor Rozhentsov <egor.rozhentsov@gmail.com>
2020-01-28 11:27:33 +0300

    Added opening modes

--- framework/requirements_manager.rb
+++ framework/requirements_manager.rb
@@ -1,13 +1,14 @@
 # frozen_string_literal: true
 
 require 'appium_lib'
+require 'fileutils'
 require 'json'
 require 'logger'
 
-require_relative 'adb'
+require_relative 'adb_wrapper'
 require_relative 'appium_extension'
 require_relative 'appium_server'
-require_relative 'common_methods'
 require_relative 'config_helper'
 require_relative 'device'
-require_relative 'smoke'
+require_relative 'file_management'
+require_relative 'opening_mode'
