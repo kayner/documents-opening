2345064e240bfa43de5255dd82f8aa1106be1db2
Egor Rozhentsov <egor.rozhentsov@gmail.com>
2020-01-29 13:34:33 +0300

    Resource-id string moved to module ID

---
+++ framework/id.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+# Module for storing resource-id values
+module ID
+  ON_BOARDING_SKIP_BTN = 'on_boarding_panel_skip_button'
+  NAVIGATION_ON_DEVICE_BTN = 'menu_item_on_device'
+  SEARCH_OPEN_BTN = 'search_button'
+  SEARCH_FIELD = 'search_src_text'
+  FILE_NAME = 'list_explorer_file_name'
+  EDITORS_ADD_BTN = 'toolbarAddButton'
+  EDITORS_SETTINGS_BTN = 'toolbarSettingsButton'
+
+  EXPORT_DOCUMENT_BTN = 'docsSettingsItemDownload'
+  EXPORT_DOCUMENT_TO_DOCX = 'docsSaveDocx'
+  EXPORT_DOCUMENT_TO_PDF = 'docsSavePdf'
+  EXPORT_DOCUMENT_TO_RTF = 'docsSaveRtf'
+  EXPORT_DOCUMENT_TO_ODF = 'docsSaveOdf'
+
+  EXPORT_SPREADSHEET_BTN = 'cellsSettingsItemDownload'
+  EXPORT_SPREADSHEET_TO_XLSX = 'cellsSaveXlsx'
+  EXPORT_SPREADSHEET_TO_PDF = 'cellsSavePdf'
+  EXPORT_SPREADSHEET_TO_ODS = 'cellsSaveOds'
+  EXPORT_SPREADSHEET_TO_CSV = 'cellsSaveCsv'
+
+  EXPORT_PRESENTATION_BTN = 'slidesSettingsItemDownload'
+  EXPORT_PRESENTATION_TO_PPTX = 'slidesSavePptx'
+  EXPORT_PRESENTATION_TO_PDF = 'slidesSavePdf'
+  EXPORT_PRESENTATION_TO_ODP = 'slidesSaveOdp'
+end
