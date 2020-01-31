f3bd2fbd58e4b4c93fb1d3bf312fe09e62d777dd
Egor Rozhentsov <egor.rozhentsov@gmail.com>
2020-01-31 10:59:30 +0300

    Fixed dump helper

--- framework/helpers/dump.rb
+++ framework/helpers/dump.rb
@@ -7,23 +7,25 @@ module Helper
     # @return [DumpCreator] instance
     def initialize(path)
       @path = path
+      @dump_time = Time.now.strftime '%Y-%m-%d %H:%M:%S'
     end
 
     # @param [String] path, Path for dumping
     # @param [String] dump_path, Path for saving dump
     def perform(path, dump_path)
-      FileUtils.copy path, dump_path
+      FileUtils.cp_r path, dump_path
     end
 
     # @param [Array[String]] path_list, List of paths for dumping
     def perform_each(path_list)
       target_path = generate_dump_path
+      FileUtils.mkdir_p target_path
       path_list.each { |path| perform(path, target_path) }
     end
 
     # @return [String] Path to folder for dump save
     def generate_dump_path
-      folder_name = Time.now.strftime '%Y-%m-%d %H:%M:%S'
+      folder_name = @dump_time
       File.join @path, folder_name
     end
   end
