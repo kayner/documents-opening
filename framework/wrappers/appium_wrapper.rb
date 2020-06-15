1b5153f264aecb5549ee49f7e4ed242c53351b47
Egor Rozhentsov <egor.rozhentsov@gmail.com>
2020-06-15 20:37:43 +0300

    Updated AppiumWrapper for new parallel model

--- framework/wrappers/appium_wrapper.rb
+++ framework/wrappers/appium_wrapper.rb
@@ -1,70 +1,30 @@
 # frozen_string_literal: true
 
-# Module for extending Appium functional
 module AppiumWrapper
-  DEFAULT_TIME = 30
+  DEFAULT_TTS = 30
 
-  # @param [Selenium::WebDriver] driver, Instance of Appium::Driver
-  # @param [String] id, Id selector
-  # @param [Integer] time, Time for searching elements
-  def click(driver:, id:, time: DEFAULT_TIME)
-    driver.wait_true(time) do
-      driver.find_element(id: id).click
+  def click(id:, time: DEFAULT_TTS)
+    wait_true(time) do
+      return if find_element(id: id).click
     end
   end
 
-  # @param [Selenium::WebDriver] driver, Instance of Appium::Driver
-  # @param [String] id, Id selector
-  # @param [Integer] time, Time for searching elements
-  def element(driver:, id:, time: DEFAULT_TIME)
+  def fill(id:, data:, time: DEFAULT_TTS)
+    wait_true(time) do
+      return if find_element(id: id).send_keys data
+    end
+  end
+
+  def element_exist?(id:, time: DEFAULT_TTS)
     time.times do
-      target = driver.find_elements(id: id)
-      return target if target.count.positive?
-
-      sleep 1
-    end
-    nil
-  end
-
-  # @param [Selenium::WebDriver] driver, Instance of Appium::Driver
-  # @param [String] id, Id selector
-  # @param [Integer] time, Time for searching elements
-  # @param [String] data, Data for sending to fill
-  def fill(driver:, id:, data:, time: DEFAULT_TIME)
-    driver.wait_true(time) do
-      driver.find_element(id: id).send_keys data
-    end
-  end
-
-  # @param [Selenium::WebDriver] driver, Instance of Appium::Driver
-  # @param [String] id, Id selector
-  # @param [Integer] time, Time for searching elements
-  def elements(driver:, id:, time: DEFAULT_TIME)
-    driver.wait_true(time) do
-      driver.find_elements(id: id)
-    end
-  end
-
-  # @param [Selenium::WebDriver] driver, Instance of Appium::Driver
-  def close_keyboard(driver:)
-    driver.hide_keyboard if driver.is_keyboard_shown
-  end
-
-  # @param [Selenium::WebDriver] driver, Instance of Appium::Driver
-  def hardback(driver:)
-    driver.press_keycode 4
-  end
-
-  # @param [Selenium::WebDriver] driver, Instance of Appium::Driver
-  # @param [String] id, Id selector
-  # @param [Integer] time, Time for searching elements
-  # @return [Boolean] element existing status
-  def element_exist?(driver:, id:, time: DEFAULT_TIME)
-    time.times do
-      return true if driver.find_elements(id: id).count.positive?
+      return true if find_elements(id: id).count.positive?
 
       sleep 1
     end
     false
+  end
+
+  def hardback
+    press_keycode 4
   end
 end
