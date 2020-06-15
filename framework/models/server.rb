9427a2564589e7aca0db9fc3286a76799ea8c7dd
Egor Rozhentsov <egor.rozhentsov@gmail.com>
2020-06-15 20:38:13 +0300

    Updated Server for new config schema

--- framework/models/server.rb
+++ framework/models/server.rb
@@ -1,24 +1,18 @@
 # frozen_string_literal: true
 
-# Class for creating Appium Server
+# Class for working with Appium Server
 class Server
-  attr_reader :appium_port, :system_port, :udid
+  attr_reader :appium_port, :port, :udid
 
-  @@appium_port = 8500
-  @@system_port = 8700
-
-  # @param [String] udid, Device udid
-  # @return [AppiumServer] instance
-  def initialize(udid)
+  def initialize(udid, appium_port, system_port)
     @udid = udid
-    @appium_port = @@appium_port
-    @system_port = @@system_port
-    @@appium_port += 1
-    @@system_port += 1
+    @appium_port = appium_port
+    @port = system_port
   end
 
   def run
-    system "x-terminal-emulator -e appium -p #{@appium_port} -dc \"{\\\"udid\\\":"\
-            " \\\"#{@udid}\\\", \\\"systemPort\\\":#{@system_port}}\""
+    system "gnome-terminal -- appium -p #{@appium_port} -dc "\
+            "\"{\\\"udid\\\":\\\"#{@udid}\\\",\\\"systemPort\\\":#{@port}}\""
+    sleep 3
   end
 end
